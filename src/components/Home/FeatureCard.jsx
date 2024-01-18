e08c2a528729d4e6c40465a8990691142e88ea76
ayushichoudhary-19 <ayushichoudhary0016@gmail.com>
2024-01-18 15:37:50 +0530

    Added feature list on home

---
+++ src/components/Home/FeatureCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FeatureCard({icon,title,description}){
+    return(
+        <>
+        <div className="grid grid-col-1 align-center px-3 py-3 text-center">
+            <div className="flex items-center justify-center mb-3">
+                <img className="w-1/5 sm:w-1/5 md:w-1/3 lg:w-1/4 xl:w-1/5 my-2" src={icon} alt={title} />
+            </div>
+            <h1 className="text-black font-bold ">{title}</h1>
+            <p className="text-gray-400 text-sm">{description}</p>
+        </div>
+        </>
+
+);
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import FeatureCard from "./FeatureCard";
 
 export default function Home() {
     return (
+        <>
         <div className="mx-auto w-full max-w-7xl">
             <aside className="flex flex-col overflow-hidden text-black rounded-lg sm:mx-16 xl:mx-0 sm:py-16">
-                {/* First div on the left */}
                 <div className="hidden sm:block absolute pt-12 items-center justify-center">
     <img className="w-3/5 sm:w-2/5 md:w-2/3 lg:w-3/4 xl:w-3/5" src="src\assets\images\SoulNect-Logo.png" alt="image1" />
 </div>
-
                 <div className="relative max-w-screen-xl px-4 pb-20 pt-10 sm:py-10 ml-100 sm:px-6 lg:px-8 flex-col sm:flex-row">
                     <div className="max-w-xl space-y-8 text-center sm:text-right sm:ml-auto sm:w-1/2">
                     <h1 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
                         What is SoulNect?
                     </h1>
+                    <img className="justify-center lg:hidden md:hidden sm:flex w-3/5 sm:w-2/5 md:w-2/3 lg:w-3/4 xl:w-3/5" src="src\assets\images\SoulNect-Logo.png" alt="image1" />
                         <p>Soulnect is a blend of <span className="text-soft-pink font-bold">soul </span> and <span className="text-soft-orange font-bold" >connect</span>, representing the idea of connecting with oneself and others on a deeper level for emotional well-being.
                             It is a soulful mental health web application that embraces the power of connection for holistic well-being. It focuses on fostering a deep connection with oneself, others, and the world to nurture the soul and promote emotional resilience. Through its features and resources, the app aims to guide users on a transformative journey of self-discovery, self-care, and meaningful connections.
                         </p>
@@ -22,5 +23,35 @@ export default function Home() {
                 </div>
             </aside>
         </div>
+
+        <div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-center my-12">
+                        Features
+                    </h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-10 px-10 mx-10 ms:mx-3 ">
+        <FeatureCard
+        icon="src\assets\images\Home\moodlog.png"
+        title="Mood Log"
+        description="Log your moods and track them over time with an interactive graph on the dashboard, gaining insights into your emotional well-being."       />
+        <FeatureCard
+        icon="src\assets\images\Home\journal.png"
+        title="Journal"
+        description="Express yourself freely in a personal journal, with the ability to revisit and reflect on your thoughts and experiences over time."  />
+        <FeatureCard
+        icon="src\assets\images\Home\meditate.png"
+        title="Meditation"
+        description="Explore guided meditations to nurture your mind and soul, promoting mindfulness and relaxation for a balanced and centered lifestyle"    
+        />
+        <FeatureCard
+        icon="src\assets\images\Home\resources.png"
+        title="Resources"
+        description="Access a wealth of mental health resources, including free videos and blogs, designed to support your well-being and personal growth." />
+        <FeatureCard
+        icon="src\assets\images\Home\community.png"
+        title="Community"
+        description="Engage in meaningful discussions with a supportive community, fostering connections and sharing experiences for a shared journey toward emotional resilience."     />
+        </div>
+        </div>
+        </> 
     );
 }
