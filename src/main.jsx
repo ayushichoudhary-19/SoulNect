53cf94459c0db018a002e65bc706e1559c061d2e
ayushichoudhary-19 <ayushichoudhary0016@gmail.com>
2024-02-01 04:33:21 +0530

    added error404 page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './components/Layout/Layout.jsx'
 import MoodLog from './components/MoodLog/MoodLog.jsx'
 import Home from './components/Home/Home.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Error404 from './components/Error404/Error404.jsx'
 
 
 const navrouter = createBrowserRouter([
@@ -20,6 +21,10 @@ const navrouter = createBrowserRouter([
     {
       path: 'moodlog',
       element: <MoodLog/>
+    },
+    {
+      path: '*',
+      element: <Error404/>
     },
   ]
   }
